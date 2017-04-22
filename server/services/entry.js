9fc0c2ceea1bbaeec485d3dc8954c09ab705c9e2
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2017-04-22 13:18:26 +0200

    [Feature] No dupe entries

--- server/services/entry.js
+++ server/services/entry.js
@@ -1,13 +1,19 @@
 const uuid = require("node-uuid");
+const N1qlQuery = require("couchbase").N1qlQuery;
 const db = require("./couchbase");
 const readability = require("./readability");
 
 const DB_TYPE = "ENTRY";
 const generateId = () => `${DB_TYPE}::${uuid.v4()}`;
 
-// TODO: handle deduplication (createOrUpdate-like behavior ?)
 // TODO: handle multiple types of entries (only generic "article" behavior now)
 async function createFromUrl(url) {
+  const entryFromDb = await getFromUrl(url);
+
+  if (entryFromDb) {
+    return entryFromDb;
+  }
+
   const id = generateId();
   const baseEntry = Object.assign({
     creationDate: Date.now(),
@@ -18,6 +24,22 @@ async function createFromUrl(url) {
   entry = Object.assign(baseEntry, entry);
   await db.insert(id, entry);
   return Object.assign({ id }, entry);
+}
+
+async function getFromUrl(url) {
+  const idLike = `${DB_TYPE}::%`;
+  const query = N1qlQuery.fromString(
+    `
+    SELECT meta(t).id, t.*
+    FROM \`nomnom\` as t
+    WHERE meta(t).id LIKE $1
+    AND t.url = $2
+    `
+  );
+
+  const entries = await db.query(query, [idLike, url]);
+
+  return entries[0];
 }
 
 module.exports = {
