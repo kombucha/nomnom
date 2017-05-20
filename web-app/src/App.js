0fb4dc8b28f95eaf38f8c813675e1362b1b48ae7
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2017-05-20 17:41:57 +0200

    Code splitting on routes (seems a bit useless right now but hey)

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 
+import asyncComponent from "./components/asyncComponent";
 import PageTitle from "./components/PageTitle";
 import AppBar from "./components/AppBar";
 import PrivateRoute from "./components/PrivateRoute";
 import UserMenu from "./components/UserMenu";
 import Entries from "./pages/Entries";
 import Entry from "./pages/Entry";
-import Feeds from "./pages/Feeds";
 import Login from "./pages/Login";
-import Settings from "./pages/Settings";
 
 const PageContainer = styled.div`padding-top: 64px;`;
 const UnstyledLink = styled(Link)`text-decoration: none; color: inherit;`;
@@ -26,11 +25,16 @@ class App extends Component {
           rightElement={<UserMenu />}
         />
         <PageContainer>
-          <Route path="/login" component={Login} />
-          <PrivateRoute exact path="/" component={Entries} />
-          <PrivateRoute path="/entries/:entryId" component={Entry} />
-          <PrivateRoute path="/feeds" component={Feeds} />
-          <PrivateRoute path="/settings" component={Settings} />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <PrivateRoute exact path="/" component={Entries} />
+            <PrivateRoute path="/entries/:entryId" component={Entry} />
+            <PrivateRoute path="/feeds" component={asyncComponent(() => import("./pages/Feeds"))} />
+            <PrivateRoute
+              path="/settings"
+              component={asyncComponent(() => import("./pages/Settings"))}
+            />
+          </Switch>
         </PageContainer>
       </div>
     );
