bab054dc4a6caee683454a90b7e458aa297a8f8f
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-06-09 22:53:54 +0200

    Fix: feeds

--- packages/nomnom-api/jobs/index.js
+++ packages/nomnom-api/jobs/index.js
@@ -34,7 +34,7 @@ async function setupQueues() {
       logger.info(`Emptying queue '${queue.name}'...`);
       await removeAllJobs(queue);
     }
-    // return;
+    return;
   }
 
   logger.info(`Cleaning repeatable jobs...`);
@@ -43,8 +43,7 @@ async function setupQueues() {
   }
 
   logger.info(`Scheduling "${feeds.FEEDS_UPDATE}" (cron: ${EVERY_HOUR})`);
-  // feedsQueue.add(feeds.FEEDS_UPDATE, {}, { repeat: { cron: EVERY_HOUR } });
-  feedsQueue.add(feeds.FEEDS_UPDATE, {});
+  feedsQueue.add(feeds.FEEDS_UPDATE, {}, { repeat: { cron: EVERY_HOUR } });
 
   logger.info(`Scheduling maintenance (cron: ${EVERY_HOUR})`);
   maintenanceQueue.add({}, { repeat: { cron: EVERY_HOUR } });
