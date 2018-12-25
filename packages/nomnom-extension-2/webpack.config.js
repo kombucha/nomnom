3602f6b4f5bbde895d702e3ca5ab1f7af7ea8935
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-12-25 18:32:17 +0100

    wip

--- packages/nomnom-extension-2/webpack.config.js
+++ packages/nomnom-extension-2/webpack.config.js
@@ -1,10 +1,37 @@
 const path = require("path");
+
+const glob = require("glob");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+  }
+}
 
 module.exports = (_env, { mode }) => {
   const devMode = mode !== "production";
+
+  const plugins = [
+    new CleanWebpackPlugin(["dist"]),
+    new MiniCssExtractPlugin({ filename: devMode ? "[name].css" : "[name].[hash].css" }),
+    new HtmlWebpackPlugin({ template: "src/index.html" })
+  ];
+
+  if (!devMode) {
+    plugins.push(
+      new PurgecssPlugin({
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync(path.join(__dirname, "src/*")),
+        extractors: [{ extractor: TailwindExtractor, extensions: ["html", "tsx"] }]
+      })
+    );
+  }
 
   return {
     entry: "./src/index.tsx",
@@ -13,6 +40,16 @@ module.exports = (_env, { mode }) => {
       path: path.resolve(__dirname, "dist")
     },
     devServer: { contentBase: "./dist" },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
     module: {
       rules: [
         {
@@ -22,14 +59,14 @@ module.exports = (_env, { mode }) => {
         },
         {
           test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader"
+          ]
         }
       ]
     },
-    plugins: [
-      new CleanWebpackPlugin(["dist"]),
-      new MiniCssExtractPlugin({ filename: devMode ? "[name].css" : "[name].[hash].css" }),
-      new HtmlWebpackPlugin({ template: "src/index.html" })
-    ]
+    plugins
   };
 };
